1f14e32af79f8f6dfb6ca8e9d0a9d12491da4d31
Meshal Al-Sultan <51976236+meshalalsultan@users.noreply.github.com>
2020-02-22 17:42:15 +0300

    Add files via upload

---
+++ Check_for_name_script.py
@@ -0,0 +1,19 @@
+name = ['meshal', 'mesho', 'mesh', 'mshoo']
+user_name = input('type your name : ')
+age = [30]
+
+for my_age in age:
+    if my_age <= 35:
+        print('your age under 35')
+    elif my_age > 35:
+        print('your age above 35')
+    else:
+        print('your age is not any of my record ')
+
+for my_name in name:
+    if user_name in name:
+        print(f'your name {user_name} is good to go ')
+    else:
+        print(f'your name {user_name} is not good ')
+
+    break
